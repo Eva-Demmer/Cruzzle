092de7e91dafa90b81997adcb5ff7fe23f03ed14
Benoit dm <benoit.dumesnildot@gmail.com>
2023-06-10 19:50:34 +0200

    set up TS to Backend, added esLint rules, added some MVC template files

--- backend/migrate.ts
+++ backend/migrate.ts
@@ -1,14 +1,15 @@
-require("dotenv").config();
+import dotenv, { DotenvConfigOptions } from "dotenv";
+import * as fs from "fs";
+import mysql from "mysql2/promise";
 
-const fs = require("fs");
-const mysql = require("mysql2/promise");
+dotenv.config(<DotenvConfigOptions>{ silent: true });
 
 const migrate = async () => {
   const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
   const connection = await mysql.createConnection({
     host: DB_HOST,
-    port: DB_PORT,
+    port: Number(DB_PORT),
     user: DB_USER,
     password: DB_PASSWORD,
     multipleStatements: true,
