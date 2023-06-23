352875cf632f60b951d236042dbb980f9133e3e6
Olivier TraveauxE <olivier.traveaux@gmail.com>
2023-06-23 20:11:13 +0200

    modal responsive, small fix ideaCard

---
+++ frontend/src/components/modal/Modal.jsx
@@ -0,0 +1,62 @@
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+
+export function Modal({ isOpen, onClose, onSave, children }) {
+  if (!isOpen) return null;
+
+  const globalOverlay = useRef();
+  const closeButton = useRef();
+
+  const handleClose = (event) => {
+    if (
+      event.target === globalOverlay.current ||
+      event.target === closeButton.current
+    ) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      role="presentation"
+      ref={globalOverlay}
+      onClick={handleClose}
+      className="black-overlay flex items-center justify-center w-full absolute inset-0 z-[100] bg-black bg-opacity-50"
+    >
+      <div className="rounded-lg shadow-lg flex flex-col w-full m-2 max-h-[97%] overflow-scroll no-scrollbar::-webkit-scrollbar no-scrollbar sm:mx-16 lg:max-w-[800px] bg-white">
+        <div className="flex flex-col gap-4 px-2 pt-10 pb-8 sm:px-8 sm:pt-16 sm:pb-10">
+          {children}
+        </div>
+        <div className="flex gap-6 justify-center py-4 bg-white drop-shadow-top sticky bottom-0 z-[100] sm:py-6">
+          <Button
+            disableElevation
+            variant="contained"
+            sx={{ width: "125px" }}
+            onClick={onSave}
+          >
+            Save
+          </Button>
+          <Button
+            ref={closeButton}
+            disableElevation
+            variant="outlined"
+            sx={{ width: "125px" }}
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Modal;
--- frontend/src/pages/ideas/Ideas.jsx
+++ frontend/src/pages/ideas/Ideas.jsx
@@ -1,18 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import TextField from "@mui/material/TextField";
 import IdeaDisplayer from "../../components/idea/IdeaDisplayer";
 import IdeasProvider from "../../contexts/IdeasContext";
 import FilterProvider from "../../contexts/FilterContext";
 import Filterbar from "../../components/filterbar/Filterbar";
+import { Modal } from "../../components/modal/Modal";
 
 function Ideas() {
+  const [isModalTest1Open, setIsModalTest1Open] = useState(false);
+  const [ideas, setIdeas] = useState();
+  const toggleModal = () => {
+    setIsModalTest1Open(!isModalTest1Open);
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:6001/api/ideas").then((res) => {
+      setIdeas(res.data);
+      console.info(res.data);
+
+      return res.data;
+    });
+  }, []);
+
+  useEffect(() => {
+    // console.info(ideas);
+  }, [ideas]);
+
   return (
     <FilterProvider>
       <IdeasProvider>
+        <button type="button" onClick={toggleModal}>
+          open modal
+        </button>
         <div className="ideas-page w-full flex flex-col h-screen">
-          <header className="w-full px-6 min-[1439px]:w-8/12">
+          <header className="w-full px-6 min-[1439px]:w-8/12 bg-white">
             <h2>Ideas</h2>
             <Filterbar />
           </header>
-          <div className="ideas-header flex flex-row ">
+          <div className="ideas-header flex flex-row">
             <main className="ideas-main w-full min-[1439px]:w-8/12">
               <IdeaDisplayer isMini={false} />
             </main>
@@ -21,6 +47,72 @@ function Ideas() {
               <IdeaDisplayer isMini="true" />
             </aside>
           </div>
+          <Modal
+            isOpen={isModalTest1Open}
+            onClose={toggleModal}
+            onSave={() => console.info("bonjour")}
+          >
+            <TextField
+              sx={{ width: "300px" }}
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+            />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField
+              id="standard-basic"
+              label="Standard"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "300px" }}
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+            />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField
+              id="standard-basic"
+              label="Standard"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "300px" }}
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+            />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField
+              id="standard-basic"
+              label="Standard"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "300px" }}
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+            />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField
+              id="standard-basic"
+              label="Standard"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "300px" }}
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+            />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField
+              id="standard-basic"
+              label="Standard"
+              variant="standard"
+            />
+          </Modal>
         </div>
       </IdeasProvider>
     </FilterProvider>
