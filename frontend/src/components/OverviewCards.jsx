a68146d982e074027f010b1f1b9b023a0f582bc7
Eva Demmer <eva.demmer@gmail.com>
2023-06-27 11:24:09 +0200

    Created reusable component for cards in overview  - desktop

--- frontend/src/components/OverviewCards.jsx
+++ frontend/src/components/OverviewCards.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import PropTypes from "prop-types";
 
-function OverviewCards() {
-  return <div>OverviewCards</div>;
+function OverviewCards({ icon: Icon, title, value }) {
+  return (
+    <div className="h-32 w-68 shadow-md rounded-2xl flex flex-col relative">
+      {Icon && (
+        <div className="absolute top-[-18px] left-[-18px]">
+          <Icon className="h-10 w-10 text-primary-900 fill-current" />
+        </div>
+      )}
+      <div className="m-5">
+        <h3 className="text-black">{title}</h3>
+        <h2 className="text-black text-4xl">{value}</h2>
+      </div>
+    </div>
+  );
 }
+
+OverviewCards.propTypes = {
+  icon: PropTypes.elementType.isRequired,
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default OverviewCards;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,44 @@
+import { useContext } from "react";
+import {
+  LightBulbIcon,
+  PencilIcon,
+  SquaresPlusIcon,
+  TrophyIcon,
+} from "@heroicons/react/24/solid";
+import { UserContext } from "../contexts/UserContext";
 import OverviewCards from "../components/OverviewCards";
 
 function Home() {
+  const { puzzlesFinished, totalIdeas, participations } =
+    useContext(UserContext);
   return (
     <div>
-      <OverviewCards />
+      <h3 className="text-black mb-5">Overview</h3>
+      <div className="w-1/2 grid grid-cols-2 gap-x-8 gap-y-12">
+        <OverviewCards
+          icon={TrophyIcon}
+          title="Finished puzzles"
+          value={puzzlesFinished}
+        />
+        <OverviewCards
+          icon={LightBulbIcon}
+          title="Total ideas"
+          value={totalIdeas}
+        />
+        <OverviewCards
+          icon={SquaresPlusIcon}
+          title="Participations"
+          value={participations}
+        />
+        <div className="h-32 w-68 shadow-md rounded-2xl flex flex-col relative">
+          <PencilIcon className="h-10 w-10 absolute top-[-18px] left-[-18px] text-primary-900 fill-current" />
+          <div className="m-5">
+            <h3 className="text-black">Ideas created today</h3>
+            <h2 className="text-black text-4xl">?</h2>{" "}
+            {/* TODO: Replace with real value --> axios call */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
