95d3a19a24403fe6324f2d57f8bcc66aba5ada59
Benoit dm <benoit.dumesnildot@gmail.com>
2023-07-03 14:47:47 +0200

    removed console.info

--- backend/src/models/idea.model.ts
+++ backend/src/models/idea.model.ts
@@ -127,44 +127,6 @@ const findById = async (id: number) => {
   }
 };
 
-const findByFilter = async (filterQuery: IdeaFilterQuery) => {
-  const {
-    publicationDateStart,
-    publicationDateEnd,
-    autorSelectionTag,
-    selectedCategories = null,
-    trendingTag,
-    titleContains = null,
-    hasAttachment,
-    hasNoComment,
-  } = filterQuery;
-
-  console.info(publicationDateStart, dayjs(publicationDateStart).toISOString());
-  console.info(publicationDateEnd);
-  console.info(autorSelectionTag);
-  console.info(selectedCategories);
-  console.info(trendingTag);
-  console.info(titleContains);
-  console.info(hasAttachment);
-  console.info(hasNoComment);
-  try {
-    const data = await prisma.idea.findMany({
-      where: {
-        created_at: {
-          gte: dayjs(publicationDateStart).subtract(1, "day").toISOString(),
-          lte: dayjs(publicationDateEnd).toISOString(),
-        },
-      },
-      orderBy: {
-        created_at: "asc",
-      },
-    });
-    return data;
-  } finally {
-    await prisma.$disconnect();
-  }
-};
-
 const createIdea = async (dataIdea: Idea, userId: number): Promise<Idea> => {
   const { title, context, ...otherDataIdea } = dataIdea;
 
@@ -256,7 +218,6 @@ const updateIdea = async (
 export {
   findAll,
   findById,
-  findByFilter,
   createIdea,
   addPrimaryImgIdea,
   deleteIdea,
