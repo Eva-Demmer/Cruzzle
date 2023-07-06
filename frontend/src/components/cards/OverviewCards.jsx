d5c7cdd5c4651c6ceba5807b7bdd193d3844d1c9
Eva Demmer <eva.demmer@gmail.com>
2023-07-07 01:07:33 +0200

    cards overview fully responsive

---
+++ frontend/src/components/cards/OverviewCards.jsx
@@ -0,0 +1,132 @@
+// import PropTypes from "prop-types";
+
+// function OverviewCards({ icon: Icon, title, value = "N/A" }) {
+//   return (
+//     <div className="h-36 w-40 md:w-52 shadow-md rounded-2xl flex flex-col relative">
+//       {Icon && (
+//         <div className="absolute top-[-10px] md:top-[-18px] left-[-10px] md:left-[-18px]">
+//           <Icon className="h-8 md:h-10 w-8 md:w-10 text-primary-900 fill-current" />
+//         </div>
+//       )}
+//       <div className="flex flex-col justify-between h-full">
+//         <h3 className="text-black text-lg md:text-xl px-5 pt-5">{title}</h3>
+//         <h2 className="text-black text-2xl md:text-3xl px-5 pb-5">{value}</h2>
+//       </div>
+//     </div>
+//   );
+// }
+
+// OverviewCards.propTypes = {
+//   icon: PropTypes.elementType.isRequired,
+//   title: PropTypes.string.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
+
+// export default OverviewCards;
+
+import { useState, useEffect, useContext } from "react";
+import {
+  LightBulbIcon,
+  PencilIcon,
+  SquaresPlusIcon,
+} from "@heroicons/react/24/solid";
+import { Paper } from "@mui/material";
+import { apiIdeas } from "../../services/api.ideas";
+import PuzzleIcon from "../../assets/PuzzleIcon.svg";
+import { UserContext } from "../../contexts/UserContext";
+
+function OverviewCards() {
+  const { user } = useContext(UserContext);
+
+  const {
+    _count: { id, comment_like: commentLike, idea },
+  } = user;
+
+  const [ideasCreatedToday, setIdeasCreatedToday] = useState(0);
+  const route = `${id}/count`;
+
+  useEffect(() => {
+    const fetchIdeasCreatedToday = async () => {
+      try {
+        const response = await apiIdeas(route);
+        setIdeasCreatedToday(response.count);
+      } catch (error) {
+        setIdeasCreatedToday("N/A");
+        console.error("Error fetching ideas created today:", error);
+      }
+    };
+    if (id) {
+      fetchIdeasCreatedToday();
+    }
+  }, [id]);
+
+  return (
+    <div className="">
+      <h3 className="pb-10 text-black">Overview</h3>
+      <div className="md:pl-5 lg:w-3/4 xl:w-1/3 grid grid-cols-2 gap-x-8 lg:gap-x-24 gap-y-12 ">
+        <Paper
+          elevation={3}
+          className="h-36 w-36 md:w-48 lg:w-64 rounded-2xl flex flex-col relative"
+        >
+          <LightBulbIcon className="h-8 md:h-10 w-8 md:w-10 absolute top-[-10px] md:top-[-18px] left-[-10px] md:left-[-18px] text-primary-900 fill-current transform rotate-45" />
+          <div className="flex flex-col justify-between h-full">
+            <h3 className="text-black text-lg md:text-xl lg:text-2xl px-5 pt-5">
+              Total ideas
+            </h3>
+            <h2 className="text-black text-3xl md:text-4xl px-4 pb-4">
+              {idea}
+            </h2>
+          </div>
+        </Paper>
+        <Paper
+          elevation={3}
+          className="h-36 w-36 md:w-48 lg:w-64 rounded-2xl flex flex-col relative"
+        >
+          <PencilIcon className="h-8 md:h-10 w-8 md:w-10 absolute top-[-10px] md:top-[-18px] left-[-10px] md:left-[-18px] text-primary-900 fill-current" />
+          <div className="flex flex-col justify-between h-full">
+            <h3 className="text-black text-lg md:text-xl lg:text-2xl px-5 pt-5">
+              Today's ideas
+            </h3>
+            <h2 className="text-black text-3xl md:text-4xl px-4 pb-4">
+              {ideasCreatedToday}
+            </h2>
+          </div>
+        </Paper>
+        <Paper
+          elevation={3}
+          className="h-36 w-36 md:w-48 lg:w-64 rounded-2xl flex flex-col relative"
+        >
+          <SquaresPlusIcon className="h-8 md:h-10 w-8 md:w-10 absolute top-[-10px] md:top-[-18px] left-[-10px] md:left-[-18px] text-primary-900 fill-current" />
+          <div className="flex flex-col justify-between h-full">
+            <h3 className="text-black text-lg md:text-xl lg:text-2xl px-5 pt-5">
+              Participations
+            </h3>
+            <h2 className="text-black text-3xl md:text-4xl px-4 pb-4">
+              {commentLike}
+            </h2>
+          </div>
+        </Paper>
+        <Paper
+          elevation={3}
+          className="h-36 w-36 md:w-48 lg:w-64 rounded-2xl flex flex-col relative"
+        >
+          <img
+            className="h-8 md:h-10 w-8 md:w-10 absolute top-[-10px] md:top-[-18px] left-[-10px] md:left-[-18px] text-primary-900 fill-current"
+            alt="puzzle"
+            src={PuzzleIcon}
+          />
+          <div className="flex flex-col justify-between h-full">
+            <h3 className="text-black text-lg md:text-xl lg:text-2xl px-5 pt-5">
+              Finished puzzles
+            </h3>
+            <h2 className="text-black text-3xl md:text-4xl px-4 pb-4">
+              à faire
+            </h2>
+          </div>
+        </Paper>
+      </div>
+    </div>
+  );
+}
+
+export default OverviewCards;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,76 +1,60 @@
 /* eslint-disable camelcase */
-import { useState, useEffect, useContext } from "react";
-import {
-  LightBulbIcon,
-  PencilIcon,
-  SquaresPlusIcon,
-  TrophyIcon,
-} from "@heroicons/react/24/solid";
-import { UserContext } from "../contexts/UserContext";
-import { apiIdeas } from "../services/api.ideas";
-import OverviewCards from "../components/OverviewCards";
+// import { useState, useEffect, useContext } from "react";
+// import { PencilIcon } from "@heroicons/react/24/solid";
+// import { UserContext } from "../contexts/UserContext";
+// import { apiIdeas } from "../services/api.ideas";
+import OverviewCards from "../components/cards/OverviewCards";
 
 function Home() {
-  const { id, score_comment, score_idea, score_like } = useContext(UserContext);
-  const [ideasCreatedToday, setIdeasCreatedToday] = useState();
-  const route = `${id}/count`;
+  // const { id } = useContext(UserContext);
+  // const [ideasCreatedToday, setIdeasCreatedToday] = useState(0);
+  // const route = `${id}/count`;
 
-  useEffect(() => {
-    const fetchIdeasCreatedToday = async () => {
-      try {
-        const response = await apiIdeas(route);
-        setIdeasCreatedToday(response.count);
-      } catch (error) {
-        setIdeasCreatedToday("N/A");
-        console.error("Error fetching ideas created today:", error);
-      }
-    };
-    if (id) {
-      fetchIdeasCreatedToday();
-    }
-  }, [id]);
+  // useEffect(() => {
+  //   const fetchIdeasCreatedToday = async () => {
+  //     try {
+  //       const response = await apiIdeas(route);
+  //       setIdeasCreatedToday(response.count);
+  //     } catch (error) {
+  //       setIdeasCreatedToday("N/A");
+  //       console.error("Error fetching ideas created today:", error);
+  //     }
+  //   };
+  //   if (id) {
+  //     fetchIdeasCreatedToday();
+  //   }
+  // }, [id]);
 
-  if (
-    score_comment === undefined ||
-    score_idea === undefined ||
-    score_like === undefined
-  ) {
-    return <div>Loading...</div>;
-  }
+  // if (
+  //   score_comment === undefined ||
+  //   score_idea === undefined ||
+  //   score_like === undefined
+  // ) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
-    <div className="h-screen p-5 lg:w-3/5 xl:w-2/5">
-      <h3 className="text-black mb-5">Overview</h3>
-      <div className="p-2">
-        <div className="grid grid-cols-2 gap-x-8 gap-y-12 md:gap-y-8 lg:gap-x-16">
-          <OverviewCards
-            icon={TrophyIcon}
-            title="Finished puzzles"
-            value={score_comment}
-          />
-          <OverviewCards
-            icon={LightBulbIcon}
-            title="Total ideas"
-            value={score_idea}
-          />
-          <OverviewCards
-            icon={SquaresPlusIcon}
-            title="Participations"
-            value={score_like}
-          />
-          <div className="h-36 w-40 md:w-52 shadow-md rounded-2xl flex flex-col relative">
-            <PencilIcon className="h-8 md:h-10 w-8 md:w-10 absolute top-[-10px] md:top-[-18px] left-[-10px] md:left-[-18px] text-primary-900 fill-current" />
-            <div className="flex flex-col justify-between h-full">
-              <h3 className="text-black text-lg md:text-xl px-5 pt-5">
-                Ideas created today
-              </h3>
-              <h2 className="text-black text-2xl md:text-3xl px-5 pb-5">
-                {ideasCreatedToday}
-              </h2>
-            </div>
-          </div>
-        </div>
-      </div>
+    // <div className="h-screen p-5 lg:w-3/5 xl:w-2/5">
+    //   <h3 className="text-black mb-5">Overview</h3>
+    //   <div className="p-2">
+    //     <div className="grid grid-cols-2 gap-x-8 gap-y-12 md:gap-y-8 lg:gap-x-16">
+    //       <OverviewCards />
+    //       <div className="h-36 w-40 md:w-52 shadow-md rounded-2xl flex flex-col relative">
+    //         <PencilIcon className="h-8 md:h-10 w-8 md:w-10 absolute top-[-10px] md:top-[-18px] left-[-10px] md:left-[-18px] text-primary-900 fill-current" />
+    //         <div className="flex flex-col justify-between h-full">
+    //           <h3 className="text-black text-lg md:text-xl px-5 pt-5">
+    //             Ideas created today
+    //           </h3>
+    //           <h2 className="text-black text-2xl md:text-3xl px-5 pb-5">
+    //             {ideasCreatedToday}
+    //           </h2>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="h-screen p-5">
+      <OverviewCards />
     </div>
   );
 }
