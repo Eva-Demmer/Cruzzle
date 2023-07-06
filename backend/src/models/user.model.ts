03054edaeb2dfe31883366828d2b7634e99396c1
Benoit dm <benoit.dumesnildot@gmail.com>
2023-07-06 17:51:05 +0200

    login setUser

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -78,6 +78,7 @@ const findByMail = async (mail: string) => {
         mail,
       },
       select: {
+        id: true,
         mail: true,
         hashed_password: true,
         role_id: true,
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,35 +8,56 @@ import {
 } from "@heroicons/react/24/solid";
 import { UserContext } from "../contexts/UserContext";
 import { apiIdeas } from "../services/api.ideas";
+import { apiUserById } from "../services/api.users";
 import OverviewCards from "../components/OverviewCards";
 
 function Home() {
-  const { id, score_comment, score_idea, score_like } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
   const [ideasCreatedToday, setIdeasCreatedToday] = useState();
-  const route = `${id}/count`;
+  const [payload, setPayload] = useState();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const parts = token.split(".");
+        setPayload(JSON.parse(atob(parts[1])));
+      } catch (error) {
+        console.info("error token", error);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (payload) {
+      apiUserById(payload.id)
+        .then((res) => {
+          if (res.status === 200) {
+            setUser(res.data);
+          } else {
+            console.error("cannot get user by id");
+          }
+        })
+        .catch((err) => {
+          console.error("error get user by id", err);
+        });
+    }
+  }, [payload]);
 
   useEffect(() => {
     const fetchIdeasCreatedToday = async () => {
       try {
-        const response = await apiIdeas(route);
+        const response = await apiIdeas(`${user.id}/count`);
         setIdeasCreatedToday(response.count);
       } catch (error) {
         setIdeasCreatedToday("N/A");
         console.error("Error fetching ideas created today:", error);
       }
     };
-    if (id) {
+    if (user.id) {
       fetchIdeasCreatedToday();
     }
-  }, [id]);
-
-  if (
-    score_comment === undefined ||
-    score_idea === undefined ||
-    score_like === undefined
-  ) {
-    return <div>Loading...</div>;
-  }
+  }, [user.id]);
 
   return (
     <div className="h-screen p-5 lg:w-3/5 xl:w-2/5">
@@ -46,17 +67,13 @@ function Home() {
           <OverviewCards
             icon={TrophyIcon}
             title="Finished puzzles"
-            value={score_comment}
+            value={10}
           />
-          <OverviewCards
-            icon={LightBulbIcon}
-            title="Total ideas"
-            value={score_idea}
-          />
+          <OverviewCards icon={LightBulbIcon} title="Total ideas" value={10} />
           <OverviewCards
             icon={SquaresPlusIcon}
             title="Participations"
-            value={score_like}
+            value={10}
           />
           <div className="h-36 w-40 md:w-52 shadow-md rounded-2xl flex flex-col relative">
             <PencilIcon className="h-8 md:h-10 w-8 md:w-10 absolute top-[-10px] md:top-[-18px] left-[-10px] md:left-[-18px] text-primary-900 fill-current" />
