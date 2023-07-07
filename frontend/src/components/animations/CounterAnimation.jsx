d149cd533570d034f3f01b020f84f2d1debf58f8
Eva Demmer <eva.demmer@gmail.com>
2023-07-07 18:36:21 +0200

    first draft of insipiration cards

---
+++ frontend/src/components/animations/CounterAnimation.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+function CountAnimation({ targetCount }) {
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    const duration = 3000;
+    const step = Math.ceil(targetCount / (duration / 10));
+    let currentCount = 1;
+
+    const timer = setInterval(() => {
+      currentCount += step;
+      if (currentCount >= targetCount) {
+        clearInterval(timer);
+        currentCount = targetCount;
+      }
+      setCount(currentCount);
+    }, 10);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [targetCount]);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-5xl text-primary-900">{count}</div>
+    </div>
+  );
+}
+
+CountAnimation.propTypes = {
+  targetCount: PropTypes.number.isRequired,
+};
+
+export default CountAnimation;
--- frontend/src/components/cards/InspirationCards.jsx
+++ frontend/src/components/cards/InspirationCards.jsx
@@ -1,8 +1,14 @@
 import { Paper, Divider } from "@mui/material";
+import {
+  HandThumbUpIcon,
+  ChatBubbleBottomCenterTextIcon,
+} from "@heroicons/react/24/solid";
 import Trophy from "../../assets/dashboard/Trophy.png";
 import medalGold from "../../assets/dashboard/Medal_gold.png";
 import medalSilver from "../../assets/dashboard/Medal_silver.png";
 import medalBronze from "../../assets/dashboard/Medal_bronze.png";
+import AvatarDoghnut from "../avatar/AvatarDoghnut";
+import CountAnimation from "../animations/CounterAnimation";
 
 function InspirationCards() {
   return (
@@ -37,11 +43,32 @@ function InspirationCards() {
       {/* Next level card */}
       <Paper elevation={3} className="h-80 w-56 rounded-2xl">
         <div className="flex flex-col px-5 mt-5">
-          <span className="pl-3 text-secondary-600">
+          <div className="h-20">
+            <AvatarDoghnut />
+          </div>
+          <span className="pl-3 pb-5 text-secondary-600">
             points until next level
           </span>
-          <span className="pl-3 text-secondary-600">likes received</span>
-          <span className="pl-3 text-secondary-600">comments received</span>
+          {/* <div className="my-5 flex flex-col items-center relative">
+            <HandThumbUpIcon className="w-10 text-primary-50 absolute top-[-14px] left-12 opacity-40" />
+            <span className="text-2xl">184</span>
+            <span className="pl-3 text-secondary-600">likes received</span>
+          </div> */}
+          {/* <div className="my-5 flex flex-col items-center relative">
+            <ChatBubbleBottomCenterTextIcon className="w-10 text-primary-50 absolute top-[-14px] right-10 opacity-40" />
+            <span className="text-2xl">375</span>
+            <span className="pl-3 text-secondary-600">comments received</span>
+          </div> */}
+          <div className="mb-8 flex flex-col items-center relative">
+            <HandThumbUpIcon className="w-24 text-primary-50 absolute top-[-18px] opacity-20" />
+            <span className="text-4xl">184</span>
+            <span className="pl-3 text-secondary-600">likes received</span>
+          </div>
+          <div className="flex flex-col items-center relative">
+            <ChatBubbleBottomCenterTextIcon className="w-20 text-primary-50 absolute top-[-10px] opacity-20" />
+            <span className="text-4xl">375</span>
+            <span className="pl-3 text-secondary-600">comments received</span>
+          </div>
         </div>
       </Paper>
       {/* Team activity card */}
@@ -50,14 +77,15 @@ function InspirationCards() {
           <h3 className="text-black text-lg md:text-xl lg:text-2xl px-5 pt-6 text-center">
             Team activity
           </h3>
-          <div className="flex items-center justify-center">
-            <Paper elevation={3} className="h-20 w-20 rounded-full">
-              <span className="text-primary-900 text-4xl font-bold flex items-center justify-center">
-                10
-              </span>
+          <div className="py-5 flex items-center justify-center">
+            <Paper
+              elevation={3}
+              className="h-40 w-40 rounded-full flex items-center justify-center"
+            >
+              <CountAnimation targetCount={10682} />
             </Paper>
           </div>
-          <span className="pl-3 text-secondary-600">
+          <span className=" text-secondary-600 flex items-center justify-center">
             ideas created on Cruzzle
           </span>
         </div>
