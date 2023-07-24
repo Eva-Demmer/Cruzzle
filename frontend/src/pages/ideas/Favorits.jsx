c5347961445357b8bc4ccc951e6a711c3ff2ace5
Olivier TraveauxE <olivier.traveaux@gmail.com>
2023-07-24 11:09:13 +0200

    add no favorits img

--- frontend/src/pages/ideas/Favorits.jsx
+++ frontend/src/pages/ideas/Favorits.jsx
@@ -10,10 +10,12 @@ import { FilterFavoritesContext } from "../../contexts/FilterFavoritesContext";
 import { fetchByQuery, fetchAll } from "../../services/api.services";
 import { sm } from "../../utils/mediaQueries";
 import { MenuContext } from "../../contexts/MenuContext";
+import empty from "../../assets/emptyFavorites.svg";
 
 function Favorits() {
   const { t } = useTranslation();
   const [trendIdeas, setTrendIdeas] = useState();
+  const [isLoading, setIsloading] = useState(false);
   const { activeMenu } = useContext(MenuContext);
   const smallQuery = useMediaQuery(sm.query);
   const { user } = useContext(UserContext);
@@ -50,6 +52,7 @@ function Favorits() {
       hasAttachment,
       hasNoComment,
     };
+    setIsloading(true);
 
     fetchByQuery("/api/favorits/filter", reqItems)
       .then((data) => {
@@ -57,6 +60,7 @@ function Favorits() {
         data.map((item) => favoritArray.push(item.idea));
 
         setFavoritesFiltered(favoritArray);
+        setIsloading(false);
       })
       .catch((error) =>
         console.error("error from api.services.fetcherByQuery", error)
@@ -93,27 +97,40 @@ function Favorits() {
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = getTrendsCategories();
-        const cardArr = await Promise.all(
-          data.map((id) => fetchAll(`/api/ideas/trends/${id}`))
+    if (favoritesFiltered.length > 0) {
+      const fetchData = async () => {
+        try {
+          const data = getTrendsCategories();
+          const cardArr = await Promise.all(
+            data.map((id) => fetchAll(`/api/ideas/trends/${id}`))
+          );
+
+          const uniqueIdeas = [
+            ...new Map(cardArr.map((idea) => [idea.id, idea])).values(),
+          ];
+          setTrendIdeas(uniqueIdeas);
+        } catch (error) {
+          console.error("error from api.services.fetcherByQuery", error);
+        }
+      };
+      fetchData();
+    } else {
+      fetchAll("/api/ideas/trends")
+        .then((data) => {
+          setTrendIdeas(data);
+        })
+        .catch((error) =>
+          console.error("error from api.services.fetcherByQuery", error)
         );
-
-        const uniqueIdeas = [
-          ...new Map(cardArr.map((idea) => [idea.id, idea])).values(),
-        ];
-        setTrendIdeas(uniqueIdeas);
-      } catch (error) {
-        console.error("error from api.services.fetcherByQuery", error);
-      }
-    };
-
-    fetchData();
+    }
   }, [favoritesFiltered]);
 
   return (
-    <div className="ideas-page w-full flex flex-col">
+    <div
+      className={`${
+        favoritesFiltered.length > 0 ? "h-min" : "h-full"
+      } ideas-page w-full flex flex-col`}
+    >
       <header
         className={`w-full px-6 sticky top-[66px] z-50 xl:w-8/12 sm:top-[62px] bg-white ${
           activeMenu && !smallQuery ? "hidden" : ""
@@ -122,16 +139,25 @@ function Favorits() {
         <h2>{t("pages.ideas.favorites.title")}</h2>
         <FilterbarFavorites />
       </header>
-      <div className="ideas-header flex flex-row ">
-        <main className="ideas-main w-full xl:w-8/12">
-          {favoritesFiltered ? (
+      <div className="ideas-header flex grow flex-row">
+        <main className="ideas-main h-full grow w-full xl:w-8/12">
+          {favoritesFiltered.length > 0 ? (
             <IdeaDisplayer
               setUpdate={setUpdate}
               ideas={favoritesFiltered}
               isMini={false}
             />
           ) : (
-            ""
+            !isLoading && (
+              <div className="flex flex-col h-full justify-center items-center gap-4">
+                <img
+                  src={empty}
+                  alt={t("alts.noFavoritesImg")}
+                  className="w-full px-8 sm:w-1/2"
+                />
+                <h3>{t("pages.ideas.favorites.empty")}</h3>
+              </div>
+            )
           )}
         </main>
         <aside className="w-4/12 hidden pl-4 pr-4 xl:inline xl:sticky xl:top-[62px] xl:right-0 xl:h-min">
